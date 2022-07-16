67f6025bef6d08a5af3dd4cd457b09ab4580a912
strozw <strozw@users.noreply.github.com>
2022-07-16 21:43:57 +0900

    add `dev-swc` & `build-swc` script

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import url from 'node:url'
 import path from 'node:path'
 
-import { Configuration } from 'webpack'
+import { Configuration, ModuleOptions, RuleSetRule } from 'webpack'
 import 'webpack-dev-server'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
@@ -10,86 +10,92 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 
-const config: Configuration = {
-  experiments: {
-    lazyCompilation: isDevelopment
-      ? {
-          imports: true,
-          entries: false,
-        }
-      : false,
-    outputModule: true,
-    topLevelAwait: true,
-  },
+const config: (env: { loader: 'babel' | 'swc' }) => Configuration = env => {
+  return {
+    experiments: {
+      lazyCompilation: isDevelopment
+        ? {
+            imports: true,
+            entries: false,
+          }
+        : false,
+      outputModule: true,
+      topLevelAwait: true,
+    },
 
-  entry: {
-    main: './src/index.tsx',
-  },
+    entry: {
+      main: './src/index.tsx',
+    },
 
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-  },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, './dist'),
+    },
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
 
-  devServer: {
-    hot: true,
-  },
+    devServer: {
+      hot: true,
+    },
 
-  module: {
-    rules: [
-      // {
-      //   test: /\.[jt]sx?$/,
-      //   exclude: /node_modules/,
-      //   loader: 'babel-loader',
-      //   options: {
-      //     plugins: [isDevelopment && 'react-refresh/babel'].filter(Boolean),
-      //   },
-      // },
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'swc-loader',
-            options: {
-              jsc: {
-                transform: {
-                  react: {
-                    pragma: 'React.createElement',
-                    pragmaFrag: 'React.Fragment',
-                    throwIfNamespace: true,
-                    development: isDevelopment,
-                    refresh: isDevelopment,
-                    runtime: 'automatic',
+    // cache: {
+    //   type: 'memory',
+    // },
+
+    module: {
+      rules: [
+        env.loader === 'swc'
+          ? {
+              test: /\.[jt]sx?$/,
+              exclude: /node_modules/,
+              use: [
+                {
+                  loader: 'swc-loader',
+                  options: {
+                    jsc: {
+                      transform: {
+                        react: {
+                          development: isDevelopment,
+                          refresh: isDevelopment,
+                          runtime: 'automatic',
+                        },
+                      },
+                    },
                   },
                 },
+              ],
+            }
+          : {
+              test: /\.[jt]sx?$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+              options: {
+                plugins: [isDevelopment && 'react-refresh/babel'].filter(
+                  Boolean
+                ),
               },
             },
-          },
-        ],
-      },
-    ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-      scriptLoading: 'module',
-    }),
-    (isDevelopment && new ReactRefreshWebpackPlugin()) as any,
-  ].filter(Boolean),
-
-  optimization: {
-    chunkIds: 'named',
-    splitChunks: {
-      chunks: 'all',
+      ],
     },
-  },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body',
+        scriptLoading: 'module',
+      }),
+      (isDevelopment && new ReactRefreshWebpackPlugin()) as any,
+    ].filter(Boolean),
+
+    optimization: {
+      chunkIds: 'named',
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  }
 }
 
 export default config
